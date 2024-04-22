f80737421c186fc208124ff24c767999c5cef3a5
a23axehe <a23axehe@student.his.se>
2024-04-22 15:14:32 +0200

    upgraded sdk versions and added export line in AndroidManifest.xml. created new class called MountainData.java yet to be filled with relevant code

---
+++ app/src/main/java/com/example/networking/MountainData.java
@@ -0,0 +1,15 @@
+package com.example.networking;
+
+import androidx.annotation.NonNull;
+
+public class MountainData {
+    private int elevation;
+    private String name;
+    private String location;
+
+    @NonNull
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
