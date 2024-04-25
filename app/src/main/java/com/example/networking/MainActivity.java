8607b116ee40a6bd3fec144f1f47e2b6be42c2fc
a23axehe <a23axehe@student.his.se>
2024-04-25 13:26:36 +0200

    replaced the class MountainData.java with Mountain.java to make the MainActivity work. Added RecyclerView.Adapter subclass as a member variable in MainActivity. Added methods for creating ViewHolders, binding data and returning item count in the RecyclerView.Adapter subclass.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -1,22 +1,46 @@
 package com.example.networking;
 
+
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
+    private ArrayList<Mountain> mountainArrayList;
+    private RecyclerView.Adapter<RecyclerView.ViewHolder> recyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new JsonFile(this, this).execute(JSON_FILE);
+        recyclerAdapter = new RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return null;
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return 0;
+            }
+        };
+            new JsonFile(this, this).execute(JSON_FILE);
+
     }
 
     @Override
