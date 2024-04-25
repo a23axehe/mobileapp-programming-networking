0a4a1f62fbcf5c7920c64a2c306a29b780f27fea
a23axehe <a23axehe@student.his.se>
2024-04-25 13:38:53 +0200

    Created a MyAdapter.java class that contains The class MyViewHolder along with several constructors inhertied from Recylerview

---
+++ app/src/main/java/com/example/networking/MyAdapter.java
@@ -0,0 +1,34 @@
+package com.example.networking;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>{
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+
+
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
